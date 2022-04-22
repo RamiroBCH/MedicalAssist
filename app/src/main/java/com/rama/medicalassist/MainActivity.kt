61bb5517b882528056fc37ee813191e337973fa3
LuisMackenzie <principemackenzie@gmail.com>
2022-04-22 15:51:29 +0200

    Ui Fixes

--- app/src/main/java/com/rama/medicalassist/MainActivity.kt
+++ app/src/main/java/com/rama/medicalassist/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own test", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
