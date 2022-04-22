0a1d179ccfc44fa945f2f7f8b6ee9da001d8cd69
LuisMackenzie <principemackenzie@gmail.com>
2022-04-22 15:07:55 +0200

    initial push

--- app/src/main/java/com/rama/medicalassist/data/Entities.kt
+++ app/src/main/java/com/rama/medicalassist/data/Entities.kt
@@ -1,9 +1,11 @@
 package com.rama.medicalassist.data
 
 import androidx.room.Entity
-
+import androidx.room.PrimaryKey
 
 @Entity()
 data class Entities(
+    @PrimaryKey
+    val primaryKey:Int,
     val id: String
 )
